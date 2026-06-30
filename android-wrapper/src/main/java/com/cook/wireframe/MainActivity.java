814b8cb6e8422a466ed2291b76f8979ca4672628
ooooo_ori <dhtp0112@gmail.com>
2026-06-30 21:39:41 +0900

    fix(android): 앱 화면 풀스크린 표시

--- android-wrapper/src/main/java/com/cook/wireframe/MainActivity.java
+++ android-wrapper/src/main/java/com/cook/wireframe/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cook.wireframe;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -17,10 +18,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        getWindow().setStatusBarColor(Color.BLACK);
+        getWindow().setNavigationBarColor(Color.BLACK);
 
         WebView.setWebContentsDebuggingEnabled(true);
         webView = new WebView(this);
@@ -45,7 +44,7 @@ public class MainActivity extends Activity {
         settings.setMediaPlaybackRequiresUserGesture(false);
 
         setContentView(webView);
-        webView.loadUrl("file:///android_asset/app.html#home");
+        webView.loadUrl("file:///android_asset/app.html?app=1#home");
     }
 
     @Override
